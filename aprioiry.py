cd5194ef0e74aec784b97bc9c619281b6e926e83
Almonzer-Alaaeldin <bronzemonzer1@gmail.com>
2020-04-08 17:53:28 +0200

    code cleaned and organised

--- aprioiry.py
+++ aprioiry.py
@@ -1,91 +1,104 @@
 import numpy as np
 import itertools as itr
-# import pandas as pd
+
+############################# Start of Helper Functions ###############################
+
+def read_data_txt(file_path='ticdata2000.txt',data_size=(5822, 86)):
+    ''' Read Training Data size=(5822, 86)'''
+    # Prepare array for data
+    data = np.zeros(data_size)
+    
+    # Read All The Data and fill the array
+    datafile = open(file_path, 'r')
+    
+    i = 0
+    
+    for line in datafile:
+        data[i] = [eval(val) for val in line.split()]
+        i += 1
+    
+    return data
+    
+def slice_attr(full_data, selected_index=47):
+    # split for given index
+    # selected_index -= 1
+    return full_data[:, selected_index: selected_index+12]
+
+def set_apart_attr(data):
+    # Data with labeled items
+    data_U = np.empty(data.shape, dtype=np.dtype('U100'))
+    
+    # format numbers in each columns by column's number
+    for cInd in range(data.shape[1]):
+        for rInd in range(data.shape[0]):
+            data_U[rInd][cInd] =  str(int(data[rInd][cInd]))+'_'+ str(cInd)
+       
+    return data_U
+    
+
+def itemset_support(uniqueData, previous_itemsets=[], itemset_lvl=1):
+    
+    if itemset_lvl == 1:
+        itemsets_count = {}
+        for cInd in range(uniqueData.shape[1]):
+            for rInd in range(uniqueData.shape[0]):
+                val = uniqueData[rInd][cInd]
+                if val in itemsets_count.keys():
+                    itemsets_count[val] += 1
+                else:
+                    itemsets_count[val] = 0
+                    
+    elif itemset_lvl > 1:
+        itemsets_count = {}
+        for itemset in list(itr.combinations(previous_itemsets, itemset_lvl)):
+            key = ','.join(itemset)
+            itemsets_count[key] = 0
+            items_exist_together = True
+            
+            for row in uniqueData:            
+                for item in itemset:
+                    if not item in row:
+                        items_exist_together = False
+                        break
+                if items_exist_together:
+                    itemsets_count[key] += 1
+                    items_exist_together = True
+        
+    # find items with unsufficient support       
+    items_under_support = []
+    for itemset in itemsets_count.keys():
+        if (itemsets_count[itemset] / uniqueData.shape[0]) < support:
+            items_under_support.append(itemset)
+    
+    # remove items with unsufficient support
+    for itemset in items_under_support:
+        del itemsets_count[itemset]
+    
+    if len(itemsets_count.keys()) == 0:
+        # Stop Algorithm
+        return previous_itemsets
+            
+    else:
+        itemset_lvl += 1
+        itemsets = []
+        for item in itemsets_count.keys():
+            itemsets += list(set(item.split(',')) - set(itemsets))
+        
+        return itemset_support(uniqueData, itemsets, itemset_lvl)
+      
+############################# End of Helper Functions ###############################
 
 
-# Prepare array for data
-data_86 = np.zeros((5822, 86))
-
-# Read All The Data and fill the array
-datafile = open('ticdata2000.txt', 'r')
-
-i = 0
-
-for line in datafile:
-    data_86[i] = [eval(val) for val in line.split()]
-    i += 1
-
-
-#### Temporary Code ####
-# Choose Starting Index
-
+# Main Program
 SI = eval(input('Starting Index: '))
-
-print(SI)
-
-# split for given index
-data_12 = data_86[:, SI: SI+12]
-
-# User Defined Support and Confidence
 support = eval(input('Enter Support: '))
 confidence = eval(input('Enter confidence: '))
 
-#### Temporary Debugging Code ####
-print('support is: {}\nconfidence is: {}'.format(support, confidence))
+data = read_data_txt(file_path='ticdata2000.txt',data_size=(5822, 86))
 
-# Data with labeled items
-data_12L = np.empty(data_12.shape, dtype=np.dtype('U100'))
+data_12 = set_apart_attr(slice_attr(data, SI))
 
-# format numbers in each columns by column's number
-for cInd in range(data_12.shape[1]):
-    for rInd in range(data_12.shape[0]):
-        # vs data_12[cInd][rInd]
-        # print(str(int(data_12[rInd][cInd]))+'_'+ str(cInd))
-        data_12L[rInd][cInd] =  str(int(data_12[rInd][cInd]))+'_'+ str(cInd)
-        
-
-#### Debugging code ####
-# itemsets_2 = list(itr.permutations(range(1,12), 2))
-
-# for pair in itemsets_2:
-#     print (pair)
-
-# Start Counting Items and their frequencies
-itemsets1_count = {}
-
-for cInd in range(data_12L.shape[1]):
-    for rInd in range(data_12L.shape[0]):
-        val = data_12L[rInd][cInd]
-        if val in itemsets1_count.keys():
-            itemsets1_count[val] += 1
-        else:
-            itemsets1_count[val] = 0
-
-# find items with unsufficient support       
-items_under_support = []
-for item in itemsets1_count.keys():
-    if (itemsets1_count[item] / data_12L.shape[0]) < support:
-        items_under_support.append(item)
-
-# remove items with unsufficient support
-for item in items_under_support:
-    del itemsets1_count[item]
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+items = itemset_support(data_12)
         
         
         
